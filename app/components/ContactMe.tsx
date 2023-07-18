537eb8c2112ad9e4371f4b540726313ca7898c5e
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2023-07-18 20:17:40 +0530

    emailjs added, ccontact form works

--- app/components/ContactMe.tsx
+++ app/components/ContactMe.tsx
@@ -1,28 +1,15 @@
 "use client"
-import { useState } from "react"
-import Link from "next/link"
+import { useRef } from "react"
+import emailjs from "@emailjs/browser"
 
 const Contact = () => {
 
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: ""
-  });
-
-  const handleFormChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setFormData((prevFormData) => {
-      return {
-        ...prevFormData,
-        [event.target.name]: event.target.value
-      }
-    });
-  }
-
+  const form = useRef<HTMLFormElement>();
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    alert("Form submitted");
+    emailjs.sendForm('service_d6hnjig', 'template_olm8gj7', form.current as HTMLFormElement, 'NrBLYYazrNZFskqvD')
+      .then((result) => {alert(result.text)}, (error) => {alert(error.text)});
   }
 
   return (
@@ -31,10 +18,10 @@ const Contact = () => {
           <h1 className="text-[55px]">Contact Me</h1>
           <a href="mailto:contact@akshitgoyal.dev">contact@akshitgoyal.dev</a>  
         </div>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-y-1">
-            <input type="text" name="name" id="name" placeholder="Name" onChange={handleFormChange} className="formElement"/>
-            <input type="email" name="email" id="email" placeholder="Email" onChange={handleFormChange} className="formElement"/>
-            <textarea name="message" id="message" cols={30} rows={5} placeholder="Message" onChange={handleFormChange} className="formElement"></textarea>
+        <form ref={form} onSubmit={handleSubmit} className="flex flex-col gap-y-1">
+            <input type="text" name="contact_name" id="name" placeholder="Name" className="formElement"/>
+            <input type="email" name="contact_mail" id="email" placeholder="Email" className="formElement"/>
+            <textarea name="message" id="message" cols={30} rows={5} placeholder="Message" className="formElement"></textarea>
             <hr className="my-2 bg-gray-400 border-0 h-[0.1rem] mr-[15rem]" />
             <input type="submit" value="Send" className="border-2 border-gray-400 w-[7.5rem] py-[0.2rem] hover:cursor-pointer"/>
         </form>
