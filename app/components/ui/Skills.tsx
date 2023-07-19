b93069aa0aa713175192e20531dba7189d7aab33
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2023-07-19 12:52:45 +0530

    skills logo arranged

--- app/components/ui/Skills.tsx
+++ app/components/ui/Skills.tsx
@@ -3,21 +3,31 @@ import React from "react"
 import Link from "next/link"
 import Image from "next/image"
 
-import ReactLogo from "@public/skillsLogo/react.svg"
-import NextLogo from "@public/skillsLogo/next.svg"
-import JavascriptLogo from "@public/skillsLogo/js.svg"
 import HtmlLogo from "@public/skillsLogo/html.svg"
 import CssLogo from "@public/skillsLogo/css.svg"
+import JavascriptLogo from "@public/skillsLogo/js.svg"
+import ReactLogo from "@public/skillsLogo/react.svg"
+import NextLogo from "@public/skillsLogo/next.svg"
+import GitLogo from "@public/skillsLogo/git.svg"
+import MongoLogo from "@public/skillsLogo/mongodb.svg"
 
 
 const Skills = () => {
   return (
-    <div className="w-4/5">
-        <Link href=""><Image src={ReactLogo} alt="" width={50} height={50}/></Link>
-        <Link href=""><Image src={NextLogo} alt="" width={100} height={100}/></Link>
-        <Link href=""><Image src={JavascriptLogo} alt="" width={50} height={50}/></Link>
-        <Link href=""><Image src={HtmlLogo} alt="" width={50} height={50}/></Link>
-        <Link href=""><Image src={CssLogo} alt="" width={50} height={50}/></Link>
+    <div className="mx-[2rem] w-4/5 h-full flex flex-col gap-[4rem] justify-center">
+      <div className="flex gap-[4rem] items-center justify-center">
+        <Image src={HtmlLogo} alt="" width={120} height={120}/>
+        <Image src={CssLogo} alt="" width={120} height={120}/>
+        <Image src={JavascriptLogo} alt="" width={120} height={120}/>
+      </div>
+      <div className="flex gap-[6rem] items-center justify-center">
+        <Image src={ReactLogo} alt="" width={120} height={120}/>
+        <Image src={NextLogo} alt="" width={200} height={200}/>
+      </div>
+      <div className="flex gap-[6rem] items-center justify-center">
+        <Image src={GitLogo} alt="" width={120} height={120}/>
+        <Image src={MongoLogo} alt="" width={300} height={300}/>
+      </div>
     </div>
   )
 }
