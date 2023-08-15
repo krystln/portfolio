d40c3e1f811449b6b4edb53350b711a42c12f8ca
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2023-08-15 11:55:53 +0530

    Scroll btns added

---
+++ app/components/Scroll.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Scroll = () => {
+  return (
+    <div className="fixed right-10 top-0 flex flex-col justify-between h-full py-10">
+      <button className="text-4xl px-10 pb-4 rounded-full bg-stone-950 w-24 h-24">
+        &uarr;
+      </button>
+      <button className="text-4xl px-10 pb-4 rounded-full bg-stone-950 w-24 h-24">
+        &darr;
+      </button>
+    </div>
+  );
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,25 @@
-import "./styles/globals.css"
+import { MouseEventHandler } from "react";
+import "./styles/globals.css";
+import { Scroll } from "./components/Scroll";
 
 export const metadata = {
-  title: "Akshit Goyal | Developer",
-  description: "Web Developer, Open Source Enthusiast, and a Student. Looking forward for opportunities to learn and grow.",
+  title: "👋 Welcome! To my website",
+  description:
+    "Web Developer, Open Source Enthusiast, and a Student. Looking forward for opportunities to learn and grow.",
   image: "",
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body >{children}</body>
+      <body className="py-32 flex flex-col items-center justify-center">
+        {children}
+        <Scroll />
+      </body>
     </html>
-  )
+  );
 }
