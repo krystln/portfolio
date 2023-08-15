a6b1b6fd212b0b03e9bd04553f78b949f27dc832
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2023-08-15 11:55:01 +0530

    Hero Changed, pic left

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,17 +1,32 @@
-import React from "react"
-import Share from "@components/ui/Share"
+import Image from "next/image";
+import Link from "next/link";
 
 const Hero = () => {
   return (
-    <div className="w-100% h-fit mx-[4rem] my-[2rem] p-[4rem] gap-2 flex Hero">
-      <div className="justify-center">
-        <h1 className="text-[128px] font-[100] h-[8rem] w-fit p-0 m-0">Akshit</h1>
-        <h1 className="text-[128px] font-extrabold h-[10rem] w-fit m-0 p-0">GOYAL</h1>
-        <div className="text-[32px] pl-1">Front-End Developer</div>
-        <Share />
+    <div className="flex flex-col gap-10 items-center w-1/3">
+      <h5 className="text-stone-400 text-lg">
+        Passionate Web Developer Crafting Engaging Experiences
+      </h5>
+      <Image
+        src={""}
+        alt=""
+        width={200}
+        height={200}
+        className="rounded-full"
+      />
+      <p className="font-[500] text-xl text-center">
+        👋 Welcome! I'm <span className="font-[600]">Akshit Goyal</span>, a
+        full-stack web developer driven by a love for detail and a flair for
+        creating captivating web experiences. From just wire frames to beautiful
+        code, I transform ideas into digital enchantment.
+      </p>
+      <div className="flex gap-5">
+        <Link href="/">Linkedin</Link>
+        <Link href="/">Twitter</Link>
+        <Link href="/">Github</Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
