4b2e1b5cf50f685db0673aa0a036c4d1fac5afaa
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2023-09-10 20:13:23 +0530

    Mobile UI: Contact

--- app/components/ContactMe.tsx
+++ app/components/ContactMe.tsx
@@ -2,7 +2,7 @@ import ContactForm from "./ui/ContactForm";
 
 const Contact = () => {
   return (
-    <div className="flex gap-20 items-center justify-center h-[100vh] w-full">
+    <div className="flex gap-20 items-center justify-center h-[100vh] w-full max-[650px]:flex-col max-[650px]:gap-10">
       <div className="flex flex-col items-center">
         <h1 className="text-4xl font-mono font-[700] hover:cursor-default mb-4">
           Contact Me
