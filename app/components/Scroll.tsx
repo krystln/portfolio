5b5bb1b4b80b83fe135f38cd127244db07736b4f
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2023-09-10 20:01:04 +0530

    mobile ui: Scroll

--- app/components/Scroll.tsx
+++ app/components/Scroll.tsx
@@ -22,7 +22,8 @@ const Scroll = () => {
 
   return (
     <>
-      <div className="fixed right-10 top-0 flex flex-col justify-between h-full py-10 text-white">
+      {/* Desktop UI */}
+      <div className="fixed right-10 top-0 flex flex-col justify-between h-full py-10 text-white max-[900px]:hidden">
         <button
           className="text-4xl px-10 pb-4 rounded-full bg-stone-800 w-24 h-24"
           onClick={scrollUp}>
@@ -41,6 +42,30 @@ const Scroll = () => {
         <button
           className="text-4xl px-10 pb-4 rounded-full bg-stone-800 w-24 h-24"
           onClick={scrollDown}>
+          &darr;
+        </button>
+      </div>
+
+      {/* Mobile UI */}
+      <div className="fixed bottom-10 flex items-center justify-center w-full min-[900px]:hidden gap-1">
+        <button
+          className="bg-stone-800 text-2xl px-10 pb-2 rounded-l-full rounded-r-sm"
+          onClick={scrollUp}>
+          &uarr;
+        </button>
+        <button
+          onClick={switchTheme}
+          className="bg-stone-800 text-2xl px-10 py-[0.3rem] rounded-sm flex items-center justify-center">
+          <Image
+            src={theme}
+            alt={isDarkTheme ? "🌞" : "🌑"}
+            width={30}
+            height={30}
+          />
+        </button>
+        <button
+          onClick={scrollDown}
+          className="bg-stone-800 text-2xl px-10 pb-2 rounded-r-full rounded-l-sm">
           &darr;
         </button>
       </div>
